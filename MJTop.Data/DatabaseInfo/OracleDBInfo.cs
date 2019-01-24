33a3ce1215b94b1cef7a575801c6dbc947e5a0df
lztkdr <lztkdr@qq.com>
2019-01-24 10:23:53 +0800

    1. 增加 MJTop.Data.Top源码 2. 暂时注释 查询 视图与存储过程的代码，防止查询表结构报错的情况

---
+++ MJTop.Data/DatabaseInfo/OracleDBInfo.cs
@@ -0,0 +1,467 @@
+﻿using MJTop.Data.SPI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MJTop.Data.DatabaseInfo
+{
+    public class OracleDBInfo : IDBInfo
+    {
+        private DB Db { get; set; }
+
+        /// <summary>
+        /// 数据库工具
+        /// </summary>
+        public Tool Tools
+        {
+            get;
+            private set;
+        }
+
+        public OracleDBInfo(DB db)
+        {
+            this.Db = db;
+            Refresh();
+            this.Tools = new Tool(db, this);
+        }
+
+        public string DBName
+        {
+            get
+            {
+                if (Db.ConnectionStringBuilder is Oracle.ManagedDataAccess.Client.OracleConnectionStringBuilder)
+                {
+                    //127.0.0.1:1521/CTMS
+                    string source = (Db.ConnectionStringBuilder as Oracle.ManagedDataAccess.Client.OracleConnectionStringBuilder).DataSource;
+                    return Regex.Replace(source, @"(.+/)(.+)", "$2");
+                }
+                else
+                {
+                    return (Db.ConnectionStringBuilder as DDTek.Oracle.OracleConnectionStringBuilder).ServiceName;
+                }
+            }
+        }
+
+        public NameValueCollection TableComments { get; private set; } = new NameValueCollection();
+
+        public List<string> TableNames { get; private set; } = new List<string>();
+        
+        public IgCaseDictionary<TableInfo> TableInfoDict { get; private set; }
+
+        public IgCaseDictionary<List<string>> TableColumnNameDict { get; private set; }
+
+        public IgCaseDictionary<List<ColumnInfo>> TableColumnInfoDict { get; private set; }
+
+        public IgCaseDictionary<NameValueCollection> TableColumnComments { get; private set; }
+
+        private IgCaseDictionary<ColumnInfo> DictColumnInfo { get; set; }
+
+        private IgCaseDictionary<string> Dict_Table_Sequence { get; set; } = new IgCaseDictionary<string>(KeyCase.Upper);
+
+        public NameValueCollection Views { get; private set; }
+
+        public NameValueCollection Procs { get; private set; }
+
+        public List<string> DBNames { get { return DBName.TransList(); } }
+        
+        public List<string> Sequences { get; set; } = new List<string>();
+
+        public ColumnInfo this[string tableName, string columnName]
+        {
+            get
+            {
+                ColumnInfo colInfo;
+                var strKey = (tableName + "@" + columnName);
+                DictColumnInfo.TryGetValue(strKey, out colInfo);
+                return colInfo;
+            }
+        }
+
+
+        public List<string> this[string tableName]
+        {
+            get
+            {
+                List<string> colNames;
+                TableColumnNameDict.TryGetValue(tableName, out colNames);
+                return colNames;
+            }
+        }
+
+
+        public string IdentitySeqName(string tableName)
+        {
+            string seqName;
+            if (Dict_Table_Sequence.TryGetValue(tableName, out seqName))
+            {
+                return seqName;
+            }
+            return string.Empty;
+        }
+
+        public bool Refresh()
+        {
+            this.DictColumnInfo = new IgCaseDictionary<ColumnInfo>(KeyCase.Upper);
+            this.TableInfoDict = new IgCaseDictionary<TableInfo>(KeyCase.Upper);
+            this.TableColumnNameDict = new IgCaseDictionary<List<string>>(KeyCase.Upper);
+            this.TableColumnInfoDict = new IgCaseDictionary<List<ColumnInfo>>(KeyCase.Upper);
+            this.TableColumnComments = new IgCaseDictionary<NameValueCollection>(KeyCase.Upper);
+
+            string sequence_Sql = "select sequence_name from user_sequences";
+            string strSql = "Select table_Name As Name,Comments As Value From User_Tab_Comments Where table_Type='TABLE' Order By table_Name Asc";
+
+
+            string viewSql = "select view_name,text from user_views order by view_name asc";
+            //Oracle 11g 推出 LISTAGG 函数
+            string procSql = "select * from (SELECT name,LISTAGG(text,' ') WITHIN  group (order by line asc) text FROM user_source  group by name ) order by name asc";
+            
+            try
+            {
+                //查询Oracle的所有序列
+                this.Sequences = Db.ReadList<string>(sequence_Sql);
+
+                this.TableComments = Db.ReadNameValues(strSql);
+
+                //this.Views = Db.ReadNameValues(viewSql);
+
+                //this.Procs = Db.ReadNameValues(procSql);
+
+                if (this.TableComments != null && this.TableComments.Count > 0)
+                {
+                    this.TableNames = this.TableComments.AllKeys.ToList();
+
+                    List<Task> lstTask = new List<Task>();
+
+                    foreach (string tableName in this.TableNames)
+                    {
+                        Task task = Task.Run(() =>
+                        {
+                            TableInfo tabInfo = new TableInfo();
+                            tabInfo.TableName = tableName;
+                            tabInfo.TabComment = this.TableComments[tableName];
+
+                            /** 该语句，包含某列是否自增列，查询慢 **/
+
+                            strSql = @"select a.COLUMN_ID As Colorder,a.COLUMN_NAME As ColumnName,a.DATA_TYPE As TypeName,b.comments As DeText,(Case When a.DATA_TYPE='NUMBER' Then a.DATA_PRECISION When a.DATA_TYPE='NVARCHAR2' Then a.DATA_LENGTH/2 Else a.DATA_LENGTH End )As Length,a.DATA_SCALE As Scale,
+	(Case When (select Count(1)  from user_cons_columns aa, user_constraints bb where aa.constraint_name = bb.constraint_name 
+	 and bb.constraint_type = 'P' and aa.table_name = '{0}' And aa.column_name=a.COLUMN_NAME)>0 Then 1 Else 0 End
+	 ) As IsPK,(
+			 case when (select count(1) from user_triggers tri INNER JOIN user_source src on tri.trigger_Name=src.Name 
+				where (triggering_Event='INSERT' and table_name='{0}')
+			and regexp_like(text,	concat(concat('nextval\s+into\s*?:\s*?new\s*?\.\s*?',a.COLUMN_NAME),'\s+?'),'i'))>0 
+			then 1 else 0 end 
+	) As IsIdentity, 
+		Case a.NULLABLE  When 'Y' Then 1 Else 0 End As CanNull,
+		a.data_default As DefaultVal from user_tab_columns a Inner Join user_col_comments b On a.TABLE_NAME=b.table_name 
+	Where b.COLUMN_NAME= a.COLUMN_NAME   and a.Table_Name='{0}'  order by a.column_ID Asc";
+                            try
+                            {
+                                if (Db.DBType == DBType.OracleDDTek)
+                                {
+                                    strSql = strSql.Replace("'{0}'", "?");
+                                    tabInfo.Colnumns = Db.GetDataTable(strSql, new { t1 = tableName.ToUpper(), t2 = tableName.ToUpper(), t3 = tableName.ToUpper() }).ConvertToListObject<ColumnInfo>();
+
+                                }
+                                else
+                                {
+                                    strSql = strSql.Replace("'{0}'", ":" + tableName);
+                                    tabInfo.Colnumns = Db.GetDataTable(strSql, new { tableName = tableName.ToUpper() }).ConvertToListObject<ColumnInfo>();
+                                }
+
+                                List<string> lstColName = new List<string>();
+                                NameValueCollection nvcColDeText = new NameValueCollection();
+                                foreach (ColumnInfo colInfo in tabInfo.Colnumns)
+                                {
+                                    lstColName.Add(colInfo.ColumnName);
+                                    nvcColDeText.Add(colInfo.ColumnName, colInfo.DeText);
+
+                                    var strKey = (tableName + "@" + colInfo.ColumnName);
+                                    this.DictColumnInfo.Add(strKey, colInfo);
+
+                                    //自增的列，需要查询序列名称
+                                    if (colInfo.IsIdentity)
+                                    {
+                                        AddColSeq(tableName, colInfo.ColumnName);
+                                    }
+
+                                    if (colInfo.IsPK)
+                                    {
+                                        tabInfo.PriKeyColName = colInfo.ColumnName;
+                                        if (colInfo.IsIdentity)
+                                        {
+                                            tabInfo.PriKeyType = PrimaryKeyType.AUTO;
+                                        }
+                                        else
+                                        {
+                                            tabInfo.PriKeyType = PrimaryKeyType.SET;
+                                        }
+                                    }
+
+                                    Global.Dict_Oracle_DbType.TryGetValue(colInfo.TypeName, out DbType type);
+                                    colInfo.DbType = type;
+                                }
+
+                                this.TableInfoDict.Add(tableName, tabInfo);
+                                this.TableColumnNameDict.Add(tableName, lstColName);
+                                this.TableColumnInfoDict.Add(tableName, tabInfo.Colnumns);
+                                this.TableColumnComments.Add(tableName, nvcColDeText);
+                            }
+                            catch (Exception ex)
+                            {
+                                LogUtils.LogError("DB", Developer.SysDefault, ex);
+                            }
+                        });
+
+                        lstTask.Add(task);
+                        if (lstTask.Count(t => t.Status != TaskStatus.RanToCompletion) >= 50)
+                        {
+                            Task.WaitAny(lstTask.ToArray());
+                            lstTask = lstTask.Where(t => t.Status != TaskStatus.RanToCompletion).ToList();
+                        }
+                    }
+                    Task.WaitAll(lstTask.ToArray());
+                }
+
+                    
+            }
+            catch (Exception ex)
+            {
+                LogUtils.LogError("DB", Developer.SysDefault, ex);
+                return false;
+            }
+            return this.TableComments.Count == this.TableInfoDict.Count;
+        }
+
+        private void AddColSeq(string tableName,string colName)
+        {
+            tableName = (tableName ?? string.Empty).ToUpper();
+            colName = (colName ?? string.Empty).ToUpper();
+            string strSql = string.Empty;
+            if (Sequences != null && Sequences.Count > 0)
+            {
+                foreach (string  seqName in Sequences)
+                {
+                    strSql = @"select count(1) from user_triggers tri INNER JOIN user_source src on tri.trigger_Name=src.Name where (triggering_Event='INSERT' and table_name='" + tableName + "') and regexp_like(text,concat(concat('" + seqName + @"\s*?\.\s*?nextval\s+into\s*?:\s*?new\s*?\.\s*?','" + colName + @"'),'\s+?'),'i')";
+                    int res = Db.Single<int>(strSql,0);
+                    if (res > 0)
+                    {
+                        Dict_Table_Sequence[tableName] = seqName;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public Dictionary<string, DateTime> GetTableStruct_Modify()
+        {
+            string strSql = "select object_name as name ,last_ddl_time as modify_date from user_objects Where object_Type='TABLE' Order By last_ddl_time Desc";
+            return Db.ReadDictionary<string, DateTime>(strSql);
+        }
+
+
+        public bool IsExistTable(string tableName)
+        {
+            tableName = (tableName ?? string.Empty).ToUpper();
+            return TableNames.Contains(tableName);
+        }
+
+        public bool IsExistColumn(string tableName, string columnName)
+        {
+            var strKey = (tableName + "@" + columnName);
+            return DictColumnInfo.ContainsKey(strKey);
+        }
+
+
+        public string GetColumnComment(string tableName, string columnName)
+        {
+            Db.CheckTabStuct(tableName, columnName);
+            ColumnInfo colInfo = null;
+            var strKey = (tableName + "@" + columnName);
+            DictColumnInfo.TryGetValue(strKey, out colInfo);
+            return colInfo?.DeText;
+        }
+
+        public string GetTableComment(string tableName)
+        {
+            Db.CheckTabStuct(tableName);
+            return TableComments[tableName];
+        }
+
+        public List<ColumnInfo> GetColumns(string tableName)
+        {
+            Db.CheckTabStuct(tableName);
+            List<ColumnInfo> colInfos = null;
+            TableColumnInfoDict.TryGetValue(tableName, out colInfos);
+            return colInfos;
+        }
+
+        public bool SetTableComment(string tableName, string comment)
+        {
+            Db.CheckTabStuct(tableName);
+
+            tableName = (tableName ?? string.Empty).ToUpper();
+           
+            string upsert_sql = string.Empty;
+            comment = (comment ?? string.Empty).Replace("'", "");
+            try
+            {
+                upsert_sql = @"comment on table " + tableName + " is '" + comment + "'";
+                Db.ExecSql(upsert_sql);
+
+                TableComments[tableName] = comment;
+
+                var tabInfo = TableInfoDict[tableName];
+                tabInfo.TabComment = comment;
+                TableInfoDict[tableName] = tabInfo;
+            }
+            catch (Exception ex)
+            {
+                LogUtils.LogError("DB", Developer.SysDefault, ex, upsert_sql);
+                return false;
+            }
+            return true;
+        }
+
+        public bool SetColumnComment(string tableName, string columnName, string comment)
+        {
+            Db.CheckTabStuct(tableName, columnName);
+
+            tableName = (tableName ?? string.Empty).ToUpper();
+            columnName = (columnName ?? string.Empty).ToUpper();
+            string upsert_sql = string.Empty;
+            comment = (comment ?? string.Empty).Replace("'", "");
+            try
+            {
+                upsert_sql = @"comment on column " + tableName + "." + columnName + " is '" + comment + "'";
+                Db.ExecSql(upsert_sql);
+
+                List<ColumnInfo> lstColInfo = TableColumnInfoDict[tableName];
+
+                NameValueCollection nvcColDesc = new NameValueCollection();
+                lstColInfo.ForEach(t =>
+                {
+                    if (t.ColumnName.Equals(columnName))
+                    {
+                        t.DeText = comment;
+                    }
+                    nvcColDesc.Add(t.ColumnName, t.DeText);
+                });
+
+                TableColumnInfoDict.Remove(tableName);
+                TableColumnInfoDict.Add(tableName, lstColInfo);
+
+                TableColumnComments.Remove(tableName);
+                TableColumnComments.Add(tableName, nvcColDesc);
+
+                var strKey = (tableName + "@" + columnName).ToUpper();
+                ColumnInfo colInfo = DictColumnInfo[strKey];
+                colInfo.DeText = comment;
+                DictColumnInfo[strKey] = colInfo;
+
+            }
+            catch (Exception ex)
+            {
+                LogUtils.LogError("DB", Developer.SysDefault, ex, upsert_sql);
+                return false;
+            }
+            return true;
+        }
+
+        public bool DropTable(string tableName)
+        {
+            Db.CheckTabStuct(tableName);
+
+            tableName = (tableName ?? string.Empty).ToUpper();
+            string drop_sql = string.Empty;
+            try
+            {
+
+                drop_sql = "drop table " + tableName;
+                Db.ExecSql(drop_sql);
+
+                this.TableComments.Remove(tableName);
+
+                this.TableNames = TableComments.AllKeys.ToList();
+
+                this.TableInfoDict.Remove(tableName);
+                this.TableColumnInfoDict.Remove(tableName);
+                this.TableColumnComments.Remove(tableName);
+
+                var lstColName = TableColumnNameDict[tableName];
+
+                foreach (var colName in lstColName)
+                {
+                    var strKey = (tableName + "@" + colName).ToUpper();
+                    this.DictColumnInfo.Remove(strKey);
+                }
+
+                this.TableColumnNameDict.Remove(tableName);
+
+            }
+            catch (Exception ex)
+            {
+                LogUtils.LogError("DB", Developer.SysDefault, ex, drop_sql);
+                return false;
+            }
+            return true;
+        }
+
+        public bool DropColumn(string tableName, string columnName)
+        {
+            Db.CheckTabStuct(tableName, columnName);
+
+            tableName = (tableName ?? string.Empty).ToUpper();
+            columnName = (columnName ?? string.Empty).ToUpper();
+
+            var strKey = (tableName + "@" + columnName);
+            
+            string drop_sql = "alter table {0} drop column {1}";
+            try
+            {
+                drop_sql = string.Format(drop_sql, tableName, columnName);
+                Db.ExecSql(drop_sql);
+
+                this.DictColumnInfo.Remove(strKey);
+
+                var nvc = TableColumnComments[tableName];
+                nvc.Remove(columnName);
+                TableColumnNameDict[tableName] = nvc.AllKeys.ToList();
+
+                var lstColInfo = TableColumnInfoDict[tableName];
+                ColumnInfo curColInfo = null;
+                lstColInfo.ForEach(t =>
+                {
+                    if (t.ColumnName.Equals(columnName))
+                    {
+                        curColInfo = t;
+
+                        //tabInfo 对应的 主键类型和主键列 也需要 跟着修改。
+                        if (curColInfo.IsPK)
+                        {
+                            var tabInfo = TableInfoDict[tableName];
+                            tabInfo.PriKeyType = PrimaryKeyType.UNKNOWN;
+                            tabInfo.PriKeyColName = null;
+                            TableInfoDict[tableName] = tabInfo;
+                        }
+                        return;
+                    }
+                });
+                lstColInfo.Remove(curColInfo);
+                TableColumnInfoDict[tableName] = lstColInfo;
+
+            }
+            catch (Exception ex)
+            {
+                LogUtils.LogError("DB", Developer.SysDefault, ex, drop_sql);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ MJTop.Data/SPI/IDB.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+
+namespace MJTop.Data.SPI
+{
+    public interface IDB
+    {
+        //void BeginTran();
+        //void BeginTran(IsolationLevel isolationLevel);
+        //void Commit();
+        //void Rollback();
+
+
+
+        #region AOP拦截
+
+        Action<DbCommand> OnExecuting { get; set; }
+
+        Action<DbCommand, object> OnExecuted { get; set; }
+
+        Action<DbCommand, Exception> OnError { get; set; }
+
+        #endregion
+
+        TableTrigger DataChangeTriggers { get; }
+
+        NameValueCollection GetInsertExcludeColumns();
+
+        ExcludeColumn InsertExcludeColumns { get; }
+
+        NameValueCollection GetUpdateExcludeColumns();
+
+        ExcludeColumn UpdateExcludeColumns { get; }
+
+        #region Bool查询
+
+        bool TryConnect();
+
+        bool ValidateSql(string strSql, out Exception ex);
+
+
+        #endregion
+
+
+        int RunStoreProc(string storeProcName, object parameters = null);
+
+        DataTable RunStoreProcGetDT(string storeProcName, object parameters = null);
+
+        DataSet RunStoreProcGetDS(string storeProcName, object parameters = null);
+
+
+        #region 基础查询
+
+        TRet Scalar<TRet>(string strSql, TRet defRet, object parameters = null);
+
+        Dictionary<string, object> GetFirstRow(string strSql, object parameters = null);
+
+        DataTable GetDataTable(string strSql, object parameters = null);
+
+        List<DataTable> GetListTable(string strSql, object parameters = null);
+
+        DataSet GetDataSet(string strSql, object parameters = null);
+        
+        DbDataReader ExecReader(string commandText, object parameters = null, CommandType commandType = CommandType.Text);
+
+        TRet Single<TRet>(string strSql, TRet defRet, object parameters = null);
+
+        List<Dictionary<string, object>> GetListDictionary(string strSql, object parameters = null);
+
+        DataTable ReadTable(string strSql, object parameters = null);
+
+        List<TRet> ReadList<TRet>(string strSql, object parameters = null);
+        NameValueCollection ReadNameValues(string strSql, object parameters = null);
+
+        Dictionary<TKey, TValue> ReadDictionary<TKey, TValue>(string strSql, object parameters = null);
+
+        #endregion
+
+        #region 执行
+
+        int ExecSql(string strSql, object parameters = null);
+        int ExecSqlTran(string strSql, object parameters = null);
+        int ExecSqlTran(params string[] sqlCmds);
+        int ExecSqlTran(List<KeyValuePair<string, List<DbParameter>>> strSqlList);
+
+
+        #endregion
+
+        int BulkInsert(string tableName, DataTable data, int batchSize = 200000, int timeout = 60);
+
+        int BulkInsert(string tableName, DbDataReader reader, int batchSize = 200000, int timeout = 60);
+
+ 
+        #region 根据表名，列名，列相关操作
+
+        bool Exist(string tableName, string columnName, object columnValue, params object[] excludeValues);
+
+        bool Insert<DT>(DT data, string tableName, params string[] excludeColNames);
+
+        int InsertGetInt<DT>(DT data, string tableName, params string[] excludeColNames);
+
+        long InsertGetLong<DT>(DT data, string tableName, params string[] excludeColNames);
+
+        bool Update<DT>(DT data, string tableName, string pkOrUniqueColName = "Id", params string[] excludeColNames);
+
+        KeyValuePair<SaveType, bool> Save<DT>(DT data, string tableName, string pkOrUniqueColName = "Id", params string[] excludeColNames);
+
+        bool UpSingle(string tableName, string columnName, object columnValue, object pkOrUniqueValue, string pkOrUniqueColName = "Id");
+
+        bool DeleteAll(string tableName);
+
+        int Delete(string tableName, object pkColName);
+
+        int Delete(string tableName, string columnName, params object[] columnValues);
+
+        #endregion
+
+        TEntity Get<TEntity>(string tableName, object whereParameters) where TEntity : class, new();
+
+        TEntity GetById<TEntity>(string tableName, object pkValue) where TEntity : class, new();
+
+        List<TEntity> GetByIds<TEntity>(string tableName, object[] pkValues) where TEntity : class, new();
+
+        List<TEntity> GetList<TEntity>(string tableName, object whereParameters) where TEntity : class, new();
+
+        List<TEntity> GetList<TEntity>(string strSql) where TEntity : class, new();
+
+        DataTable SelectAll(string tableName, string orderbyStr = null);
+
+        DataTable SelectTop(string tableName, int top, string orderbyStr = null);
+
+        long SelectCount(string tableName, string whereAndStr = null);
+
+        DataTable SelectTable(string joinTableName, string whereStr, string orderbyStr);
+               
+        KeyValuePair<DataTable, long> GetDataTableByPager(int currentPage, int pageSize, string selColumns, string joinTableName, string whereStr, string orderbyStr);
+    }
+}
